27da0c0f9c3ef73a6ec8681b899bf389c36edcf6
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-26 17:48:12 +0530

    Initial commit

---
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -0,0 +1,17 @@
+import kotlin.concurrent.thread
+
+fun main(){
+
+    println("Main Thread Starts: ${Thread.currentThread().name}")
+
+    thread{
+        println("Fake worker thread: ${Thread.currentThread().name}")
+        Thread.sleep(1000)
+        println("Fake work finished: ${Thread.currentThread().name}")
+
+    }
+
+    println("Main Thread Ends: ${Thread.currentThread().name}")
+
+
+}
